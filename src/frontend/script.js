cd1a2acd163daadacf7b3ac42c84cbe8d77b7322
Polyograph <polyographbr@gmail.com>
2025-06-23 11:10:36 -0300

    Update script.js
    Correção de sintaxe.

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -14,7 +14,7 @@ submitBtn.addEventListener('click', async () => {
 
   try {
     const response = await fetch('https://hubstree-mvp-2.onrender.com/pipeline', {
-, {
+ 
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
